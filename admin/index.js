245c4ed6ed6cf7645e160c9999c25bda7ef1a25f
psychobunny <psycho.bunny@hotmail.com>
2014-05-16 17:00:29 -0400

    compile LESS

--- admin/index.js
+++ admin/index.js
@@ -6,23 +6,31 @@ var admin = {},
 	nconf = require('nconf'),
 	express = require('express'),
 	path = require('path'),
-	fs = require('fs');
+	fs = require('fs'),
+	winston = require('winston');
 
 admin.css = null;
 
 admin.routeStaticDirectory = function(app) {
-	app.use(nconf.get('relative_path') + '/admin', express.static(path.join(__dirname, 'less'), {
+	app.use(nconf.get('relative_path') + '/admin', express.static(path.join(__dirname, 'public'), {
 		maxAge: app.enabled('cache') ? 5184000000 : 0
 	}));
 
 	app.get('/admin/stylesheet.css', function(req, res, next) {
 		res.type('text/css').send(200, admin.css);
 	});
+
+	app.get('/admin/', function(req, res, next) {
+		res.render('../../admin/public/templates/index');
+	});
 };
 
 admin.compileCSS = function() {
 	var	parser = new (require('less').Parser)({
-		filename: 'less/index.less' 
+		filename: 'less/index.less' ,
+		paths: [
+			path.join(__dirname, 'less')
+		]
 	});
 
 	parser.parse(fs.readFileSync('./admin/less/index.less').toString(), function(err, tree) {
