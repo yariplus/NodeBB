53bbf471f8f2ca785e295460b84c9f22acf1f0f5
psychobunny <psycho.bunny@hotmail.com>
2014-05-22 16:28:10 -0400

    reorganized acp routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,31 +1,54 @@
 "use strict";
 
 
-function mainRoutes(app, middleware, controllers) {
+function generalRoutes(app, middleware, controllers) {
 	/*app.get('/admin/', middleware.admin.buildHeader, controllers.admin.home);
 	app.get('/admin/index', middleware.admin.buildHeader, controllers.admin.home);
 	app.get('/api/admin/index', controllers.admin.home);*/
-	
-	app.get('/api/admin/plugins', controllers.admin.plugins.get);
-	app.get('/api/admin/settings', controllers.admin.settings.get);
-	app.get('/api/admin/themes', controllers.admin.themes.get);
-	app.get('/api/admin/languages', controllers.admin.languages.get);
-	app.get('/api/admin/groups', controllers.admin.groups.get);
-	app.get('/api/admin/sounds', controllers.admin.sounds.get);
+
+	app.namespace('/api/admin/general', function() {
+		app.get('/home', controllers.admin.home);
+		app.get('/settings', controllers.admin.settings.get);
+		app.get('/languages', controllers.admin.languages.get);
+		app.get('/sounds', controllers.admin.sounds.get);
+	});
 }
 
-function userRoutes(app, middleware, controllers) 
-	app.get('/api/admin/users/search', controllers.admin.users.search);
-	app.get('/api/admin/users/latest', controllers.admin.users.sortByJoinDate);
-	app.get('/api/admin/users/sort-posts', controllers.admin.users.sortByPosts);
-	app.get('/api/admin/users/sort-reputation', controllers.admin.users.sortByReputation);
-	app.get('/api/admin/users', controllers.admin.users.sortByJoinDate);
+function manageRoutes(app, middleware, controllers) {
+	app.namespace('/api/admin/manage', function() {
+		app.get('/categories/active', controllers.admin.categories.active);
+		app.get('/categories/disabled', controllers.admin.categories.disabled);
+
+		app.get('/users/search', controllers.admin.users.search);
+		app.get('/users/latest', controllers.admin.users.sortByJoinDate);
+		app.get('/users/sort-posts', controllers.admin.users.sortByPosts);
+		app.get('/users/sort-reputation', controllers.admin.users.sortByReputation);
+		app.get('/users', controllers.admin.users.sortByJoinDate);
+
+		app.get('/groups', controllers.admin.groups.get);
+	});
 }
 
-function forumRoutes(app, middleware, controllers) 
-	app.get('/api/admin/categories/active', controllers.admin.categories.active);
-	app.get('/api/admin/categories/disabled', controllers.admin.categories.disabled);
+function appearanceRoutes(app, middleware, controllers) {
+	app.namespace('/api/admin/appearance', function() {
+		app.get('/themes', controllers.admin.themes.get);
+	});
 }
+
+function extendRoutes(app, middleware, controllers)  {
+	app.namespace('/api/admin/extend', function() {
+		app.get('/plugins', controllers.admin.plugins.get);
+	});
+}
+
+function advancedRoutes(app, middleware, controllers) {
+	app.namespace('/api/admin/advanced', function() {
+		app.get('/database', controllers.admin.database.get);
+		app.get('/events', controllers.admin.events.get);
+		app.get('/logger', controllers.admin.logger.get);
+	});
+}
+
 
 function apiRoutes(app, middleware, controllers) {
 	// todo, needs to be in api namespace
@@ -37,16 +60,11 @@ function apiRoutes(app, middleware, controllers) {
 	app.post('/admin/uploadgravatardefault', middleware.authenticate, controllers.admin.uploads.uploadGravatarDefault);
 }
 
-function miscRoutes(app, middleware, controllers) 
-	app.get('/api/admin/database', controllers.admin.database.get);
-	app.get('/api/admin/events', controllers.admin.events.get);
-	app.get('/api/admin/logger', controllers.admin.logger.get);
-}
-
 module.exports = function(app, middleware, controllers) {
-	mainRoutes(app, middleware, controllers);
-	userRoutes(app, middleware, controllers);
-	forumRoutes(app, middleware, controllers);
+	generalRoutes(app, middleware, controllers);
+	manageRoutes(app, middleware, controllers);
+	appearanceRoutes(app, middleware, controllers);
+	extendRoutes(app, middleware, controllers);
+	advancedRoutes(app, middleware, controllers);
 	apiRoutes(app, middleware, controllers);
-	miscRoutes(app, middleware, controllers);
 };
